5bebb82af0d3c50f3a80ecd1762ab42d3d42da2a
Justas Palumickas <jpalumickas@gmail.com>
2020-12-28 13:46:32 +0200

    Init gem

---
+++ lib/emoji-datasource.rb
@@ -0,0 +1,1 @@
+require_relative 'emoji_datasource'
---
+++ lib/emoji_datasource.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "emoji_datasource/version"
+
+module EmojiDatasource
+  class Error < StandardError; end
+  # Your code goes here...
+end
